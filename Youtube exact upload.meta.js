88c16d3adf3d8622665414c7aecab1e73fe8a88a
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-03-14 11:35:12 +0100

    Add meta file

---
+++ Youtube exact upload.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name           Youtube exact upload
+// @name:de        YouTube exakter Hochladezeitpunkt
+// @description    Adds exact upload time to youtube videos
+// @description:de Fügt YouTube-Videos den exakten Hochladezeitpunkt mit Uhrzeit hinzu
+// @require        https://cdn.jsdelivr.net/npm/hacktimer@1.1.3/HackTimer.min.js
+// @require        https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment-with-locales.min.js
+// @version        0.17
+// @match          https://www.youtube.com/*
+// @grant          none
+// @namespace      https://greasyfork.org/users/94906
+// @license        MIT
+// ==/UserScript==
