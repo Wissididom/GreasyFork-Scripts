8060455ce80610c7ad2ca620a5290fbfc7f069d5
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-03-14 09:44:56 +0000

    Remove HackTimer

--- Youtube exact upload.user.js
+++ Youtube exact upload.user.js
@@ -3,7 +3,6 @@
 // @name:de        YouTube exakter Hochladezeitpunkt
 // @description    Adds exact upload time to youtube videos
 // @description:de Fügt YouTube-Videos den exakten Hochladezeitpunkt mit Uhrzeit hinzu
-// @require        https://cdn.jsdelivr.net/npm/hacktimer@1.1.3/HackTimer.min.js
 // @require        https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.30.1/moment-with-locales.min.js
 // @version        0.17
 // @match          https://www.youtube.com/*
